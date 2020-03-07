1082044d28906a80b28eb8b323ffbb7b5fd7e888
陈月阳 <chenyueyang@chenyueyangdeMacBook-Pro.local>
2020-03-07 14:58:06 +0800

    初始化

---
+++ App/Interfaces/video/VideoServiceInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Interfaces;
+
+/**
+ * Description of MovieServiceInterface
+ *
+ * @author chenyueyang
+ */
+interface MovieServiceInterface {
+    public function play_info($v_id):array;
+}
